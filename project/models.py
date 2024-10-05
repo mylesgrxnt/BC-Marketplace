9a9b05850128e0560fbae4e5f84b7355e627a2ef
Jude <jmlascano1@gmail.com>
2024-10-05 15:00:49 -0400

    test push (models.py)

--- project/models.py
+++ project/models.py
@@ -3,6 +3,23 @@ from . import db
 
 class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+  name = db.Column(db.String(1000))
   email = db.Column(db.String(100), unique=True)
   password = db.Column(db.String(100))
-  name = db.Column(db.String(1000))
+  products = db.Column(db.String(100))
+  rating = db.Column(db.String(100))
+  location = db.Column(db.String(100))
+  isOfficial = db.Column(db.Boolean)
+
+  products = db.relationship('Product', backref='owner', lazy=True)  # one-to-many relationship
+
+class Product(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100), nullable=False)
+  desc = db.Column(db.String(2000), nullable=False)
+  price = db.Column(db.Numeric(5, 2), nullable=False)
+  meetup = db.Column(db.String(100), nullable=False)
+  preferredPayment = db.Column(db.String(1000), nullable=False)
+  condition = db.Column(db.Integer, nullable=False)
+  types = db.Column(db.Integer, nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
