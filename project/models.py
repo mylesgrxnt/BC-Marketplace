2eebd449830c90fafbaee9cbbb6069082d386977
Jude <jmlascano1@gmail.com>
2024-10-05 15:08:20 -0400

    redo db

--- project/models.py
+++ project/models.py
@@ -11,7 +11,6 @@ class User(UserMixin, db.Model):
   location = db.Column(db.String(100))
   isOfficial = db.Column(db.Boolean, nullable=True, default=False)
 
-
 class Product(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100), nullable=False)
