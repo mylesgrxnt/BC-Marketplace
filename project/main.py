2895fdc251ad8d3c1397efe63dac990e0e7c179b
yd5256 <yavuzdamkaci@gmail.com>
2024-10-05 20:39:48 -0400

    added form reading for the add item page

--- project/main.py
+++ project/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from .models import User, Product
 from . import db
@@ -14,7 +14,11 @@ def index():
 def profile():
   return render_template('profile.html', name=current_user.name)
 
-@main.route('/add_item')
+@main.route('/add_item', methods =["GET", "POST"])
 @login_required
 def add_item():
+  # string is from name field of input elements in add_item.html, the dropdowns return the index (1-based) of the option selected
+  print(request.form.get("payment")) 
+
+
   return render_template('add_item.html')
