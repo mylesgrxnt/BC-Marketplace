6e7bc625356499f298e2d3c77b9b7e8cd1ddb6d7
yd5256 <yavuzdamkaci@gmail.com>
2024-10-06 00:52:14 -0400

    very cool dont change

--- project/main.py
+++ project/main.py
@@ -1,14 +1,19 @@
-from flask import Blueprint, render_template, request, flash, Flask
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import User, Product
+from .models import User, Product, Image
 from . import db
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import (StringField, TextAreaField, IntegerField, BooleanField, DecimalField,
                      RadioField)
 from wtforms.validators import InputRequired, Length
 from flask_optional_routes import OptionalRoutes
+from base64 import b64encode
+import base64
+from io import BytesIO
 
 class ProductForm(FlaskForm):
+    image = FileField("image", validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
     name = StringField('name', validators=[InputRequired()])
     description = TextAreaField('description', validators=[InputRequired()])
     price = DecimalField('price', validators=[InputRequired()])
@@ -50,7 +55,10 @@ def add_item():
   form = ProductForm()
   if request.method == "POST":
     if form.validate_on_submit():
-      new_product = Product(name=form.name.data, desc=form.description.data, price=form.price.data, meetup=form.meetup.data, preferredPayment=form.payment.data, condition=form.condition.data, types=form.type.data, user_id=current_user.id)
+      testFile = form.image.data
+      data = testFile.read()
+      renderedTestFile = base64.b64encode(data).decode('utf-8')
+      new_product = Product(name=form.name.data, desc=form.description.data, price=form.price.data, meetup=form.meetup.data, preferredPayment=form.payment.data, condition=form.condition.data, types=form.type.data, user_id=current_user.id, image_data=data, image_rendered_data=renderedTestFile)
       db.session.add(new_product)
       db.session.commit()
     else:
